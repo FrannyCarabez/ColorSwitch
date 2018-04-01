24ea9f5482b0fc945238e756c9ec0c9afc1c406f
FrannyCarabez <37878322+FrannyCarabez@users.noreply.github.com>
2018-04-01 19:38:43 +0200

    Moving the circle

---
+++ Color-Switch Project/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	// to rotate 100 degrees every second
+	public float speed = 100f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0f, 0f, speed * Time.deltaTime); // time.deltatime to even out the differencies in frame rate
+	}
+}
